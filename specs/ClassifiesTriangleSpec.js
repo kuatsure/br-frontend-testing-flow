3b803bbcfabbee16e50303f839b0ee8d3ce298c6
Jarrett Drouillard <jarrett@thestyl.us>
2015-01-15 12:48:59 -0500

    add scalene test

--- specs/ClassifiesTriangleSpec.js
+++ specs/ClassifiesTriangleSpec.js
@@ -1,14 +1,14 @@
 /**********************************
- * Magic School Bus reminder 
+ * Magic School Bus reminder
  * Classifications:
- *  scalene: all three sides are different  
+ *  scalene: all three sides are different
  *  isosceles: two of its sides are equal
  *  equilateral:  all three sides are equal
- * 
+ *
  * A Valid Triangle exists when:
  *  a + b > c
  *  a + c > b
- *  b + c > a 
+ *  b + c > a
  **********************************/
 
 
@@ -17,5 +17,9 @@ describe('ClassifiesTriangle', function() {
     expect(ClassifiesTriangle).toBeDefined();
   });
 
-});
+  it('classifies as scalene - 4, 3, 2', function () {
+    var result = new ClassifiesTriangle().classify( 4, 3, 2 );
 
+    expect( result ).toEqual('scalene');
+  });
+});
